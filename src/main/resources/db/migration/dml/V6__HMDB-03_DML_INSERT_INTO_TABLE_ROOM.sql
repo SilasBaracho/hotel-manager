1965beb87b8ecf94cdb3c3eaf7d42018be0dae81
“SilasBaracho“ <silasbaracho@gmail.com>
2024-09-04 11:14:14 -0300

    feat: Insert tables

--- src/main/resources/db/migration/dml/V6__HMDB-03_DML_INSERT_INTO_TABLE_ROOM.sql
+++ src/main/resources/db/migration/dml/V6__HMDB-03_DML_INSERT_INTO_TABLE_ROOM.sql
@@ -4,4 +4,8 @@ INSERT INTO room (
     '7d4a25d3-39fa-43b5-9f3a-734f51ebde0f', 'bff206a0-2c4e-4b71-9e3f-28dd8cb35b9e', 150.00, 'DOUBLE', 2, 1, '101', TRUE, NOW(), NULL
 ), (
     '893abf4e-f765-4e73-a8bb-7f6b6054876b', 'bff206a0-2c4e-4b71-9e3f-28dd8cb35b9e', 200.00, 'SUITE', 4, 2, '202', TRUE, NOW(), NULL
+), (
+   'ad03597c-a903-4fae-982f-11dd18277edd', '2d5c0e4b-444c-44b3-87fd-ba870f877595', 180.00, 'DOUBLE', 2, 1, '101', TRUE, NOW(), NULL
+), (
+   'd31a060d-6dca-4775-874e-b68147422265', '2d5c0e4b-444c-44b3-87fd-ba870f877595', 220.50, 'SUITE', 4, 2, '202', TRUE, NOW(), NULL
 );
